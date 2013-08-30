e36027be6a1bd546405abba66101440cbcd59e77
carladrago <carladrago@gmail.com>
2013-08-30 14:03:42 +0200

    add recipient_data_fetch.rb

---
+++ recipient_data_fetch.rb
@@ -0,0 +1,27 @@
+require 'csv'
+require 'bigdecimal'
+require 'json'
+require "rubygems"
+require "sequel"
+
+
+# connect to an in-memory database
+DB = Sequel.postgres("farmsubsidy_development")
+
+
+# create a dataset from the recipient data
+recipient = DB[:recipients]
+i = 0
+recipient_txt = CSV.open("data/cz_recipient.txt", "r:UTF-8", :headers => true, :col_sep => ";") do |csv|
+  csv.each do |row|
+      print "." if i%100 == 0
+      recipient.insert(
+        global_recipient_id: row['globalRecipientId'],
+        zipcode: row['zipcode'],
+        name: row['name']
+      )
+    i += 1
+  end
+end
+
+
