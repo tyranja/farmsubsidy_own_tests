9da8b27e54a26b47bd6f8805ae626aeefe32d479
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-09-02 10:49:45 +0200

    refactor payment_data_fetch.rb

--- payment_data_fetch.rb
+++ payment_data_fetch.rb
@@ -6,7 +6,6 @@ require 'sequel'
 # connect to an in-memory database
 DB = Sequel.postgres("farmsubsidy_development")
 
-
 # create a dataset from the payments table
 payment = DB[:payments]
 
@@ -20,19 +19,17 @@ i = 0
 payment_txt = CSV.open("data/cz_payment.txt", "r:UTF-8", :headers => true, :col_sep => ";") do |csv|
   csv.each do |row|
       print "." if i%100 == 0
-
       # find the year_id by searching years dataset
-      year_id = years.where(:year=>row['year']).all
+      year_id = years.where(:year=>row['year']).first[:id]
 
       # find the recipient_id by searching recipient dataset
-      recipient_id = recipients.where(:global_recipient_id=>row['globalRecipientId']).all
+      recipient_id = recipients.where(:global_recipient_id=>row['globalRecipientId']).first[:id]
 
       # insert data into payments table
       payment.insert(
         amount_euro: row['amountEuro'],
-        year_id: year_id[0][:id],
-        recipient_id: recipient_id[0][:id]
-        
+        year_id: year_id,
+        recipient_id: recipient_id
       )
     i += 1
   end
