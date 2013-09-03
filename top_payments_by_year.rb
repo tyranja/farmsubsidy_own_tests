7fad163ccd6030bf6ce0be81fedcf0059f0e2e88
carladrago <carladrago@gmail.com>
2013-09-03 14:19:18 +0200

    further improvement to top_payments

--- top_payments_by_year.rb
+++ top_payments_by_year.rb
@@ -1,26 +1,35 @@
 require 'csv'
 require 'rubygems'
 require 'sequel'
+require 'logger'
 
+# top_payments will be an aray of hashes with the final results
+# eg [{rank: 1, name: "horst", amount: 234556}, {...}]
+top_payments = []
 
 # connect to an in-memory database
-DB = Sequel.postgres("farmsubsidy_performance")
+DB = Sequel.postgres("farmsubsidy_performance", :loggers => [Logger.new($stdout)])
 
 #select a year between 2004 and 2011
 p "Select a year between 2004 and 2011:"
 year_selection = gets
 
-year_id = DB.fetch("SELECT id FROM years WHERE year='#{year_selection}'").first[:id]
-p year_id
+# find the year_id from the years table
+year_id = DB[:years].where(year: year_selection.to_i).first[:id]
 
-payments_sorted = DB.fetch("SELECT * FROM payments WHERE year_id='#{year_id}' ORDER BY amount_euro DESC LIMIT 20")
-p payments_sorted.first
-p payments_sorted.all.count
+# find payments by year, order them by amount, and return the first 20
+payments_sorted = DB[:payments].where(year_id: year_id).
+																reverse_order(:amount_euro).
+																limit(20)
 
-payments_sorted.each do |hash|
-	p hash[:recipient_id]
-	recipient_name = DB.fetch("SELECT name FROM recipients WHERE id='#{hash[:recipient_id]}'").first[:name]
-	p recipient_name
+# find recipient name, create an index, shove it into top_payments hash
+payments_sorted.each_with_index do |payment, index|
+	recipient_name = DB[:recipients].where(id: payment[:recipient_id]).first[:name].gsub("\"", "")
+	top_payments << {rank: index+1, name: recipient_name, amount: payment[:amount_euro]}
 end
+
+p top_payments
+
+
 
 
