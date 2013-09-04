97aeb0b54af02434d368450e093b0935f08bc1f9
carladrago <carladrago@gmail.com>
2013-09-04 16:00:27 +0200

    add models dir and seperate files for each model

--- models/payment.rb
+++ models/payment.rb
@@ -4,17 +4,6 @@ require 'sequel'
 # connect to an in-memory database
 DB = Sequel.postgres("farmsubsidy_performance")
 
-# Create a Year model.
-class Year < Sequel::Model; 
-	one_to_many :recipients
-end
-
-# Create a Recipients model.
-class Recipient < Sequel::Model; 
-	many_to_one :years
-	one_to_many :payments
-end
-
 # Create a Payments model.
 class Payment < Sequel::Model; 
 	many_to_one :recipients
