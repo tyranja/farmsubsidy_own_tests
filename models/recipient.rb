06da1c804c48b87259b04304077edcc15f59955b
carladrago <carladrago@gmail.com>
2013-09-05 10:18:25 +0200

    start on total_payments_by_recipient in Recipient class (incomplete)

--- models/recipient.rb
+++ models/recipient.rb
@@ -8,4 +8,10 @@ DB = Sequel.postgres("farmsubsidy_performance")
 class Recipient < Sequel::Model; 
 	many_to_one :years
 	one_to_many :payments
+
+	def total_payments_by_recipient(recipient_id)
+		
+		beginning = Time.now
+
+	end
 end
