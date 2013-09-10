02722a2e704e8b25f26d66955f438f780eff0a85
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-09-10 11:40:48 +0200

    chance rspec test

--- spec/year_spec.rb
+++ spec/year_spec.rb
@@ -1,19 +1,15 @@
 require 'spec_helper'
 
-describe Year do
-  # before :each do
-  #   @year = Year.new
-  # end
+describe "Year" do
+  before :each do
+    @year = Year.all
+  end
 
-  # describe "count" do
-  #   it "has 14 years" do
-  #       @year.should be_an_instance_of Year
-  #   end
+  describe "count" do
+    it "has 14 years" do
+        @year.should be_an_instance_of Year
+    end
 
-  # end
-
-  describe Year do
-    it{ should have_column :year, :type => Integer }
   end
 
 end
